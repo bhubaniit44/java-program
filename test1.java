e96d008b815639514a8a703bf098b483638b2aa1
bhubaniit44 <u22cse1044@cit.ac.in>
2023-12-31 00:02:04 +0530

    sum three number in java

---
+++ test1.java
@@ -0,0 +1,16 @@
+package javaprograming;
+
+/**
+ * test1
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+        int num1=78;
+        int num2=65;
+        int num3=45;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+        
+    }
+}
