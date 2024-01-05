b76076b25dc86ffa8691f914b3aaa045e463f810
bhubaniit44 <u22cse1044@cit.ac.in>
2024-01-05 13:01:40 +0530

    detect whether a number entered by the user is integer or not

---
+++ test5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test5 {
+ public static void main(String[] args) {
+    System.out.println("Enter the number");
+    Scanner sc=new Scanner(System.in);
+    System.out.println(sc.hasNextInt());
+
+ }   
+}
