4f6b13c36caccd9cc0780e1cd479463b45ecaa9c
bhubaniit44 <u22cse1044@cit.ac.in>
2024-01-06 22:53:56 +0530

    comparision operators to find out whether a given number  is greater than the user entered number or not

---
+++ test7.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class test7 {
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the number:");
+    int a=sc.nextInt();
+    System.out.println(a>9);
+} 
+}
