970f1b4b95209f401c7d4885307d6045bc633055
bhubaniit44 <u22cse1044@cit.ac.in>
2024-01-06 22:42:58 +0530

    encrypt a grade by adding 8 to it .Decrypt it to show the correct grade

---
+++ test6.java
@@ -0,0 +1,15 @@
+public class test6 {
+    public static void main(String[] args) {
+      char grade='B';
+      grade=(char)(grade+8);
+      System.out.println(grade);
+
+      //Decrypt it to show the correct grade
+      
+      grade=(char)(grade-8);
+      System.out.println(grade);
+
+
+
+    }
+}
